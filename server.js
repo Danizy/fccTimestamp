41f9af0d0cb2cef05d6840a1d0f82c35d8342f00
Dandol Niski <Dandol Niski>
2016-08-13 22:17:11 +0200

    Added conversion from UNIX to NATURAL

--- server.js
+++ server.js
@@ -2,9 +2,27 @@ var express = require('express')
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.sendFile(__dirname + '/site/' + 'main.html');
+})
+
+app.get('/*', function(req, res){
+
+  if(Number.isInteger(+req.params[0])){
+
+    res.send(fromUnix(req.params[0]));
+  }
+  else{
+    res.send(req.params[0]);
+  }
 })
 
 app.listen(8080, function(){
   console.log("Listening on port 8080");
 })
+
+function fromUnix(Unix){
+  var date = new Date(+Unix * 1000);
+  var mth = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var response = {natural: mth[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()};
+  return JSON.stringify(response);
+}
