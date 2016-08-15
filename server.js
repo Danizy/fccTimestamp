74e4b0982744acd7c8bcc8335d8b92ac09ea0a39
Daniel <szprytny14@gmail.com>
2016-08-15 20:19:18 +0200

    Added Natural to UNIX conversion

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express();
+var mth = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/site/' + 'main.html');
@@ -12,7 +13,19 @@ app.get('/*', function(req, res){
     res.send(fromUnix(req.params[0]));
   }
   else{
-    res.send(req.params[0]);
+    var data = req.params[0].split(' ');
+    if(dateVerify(data[0], data[1], data[2])){
+      response = {unix: null, natural: null};
+      response = JSON.stringify(response);
+      res.send(response);
+    }
+    else{
+      var date = new Date(data[0] + " " + data[1] + data[2]);
+      var response = {unix: +(date.getTime()/1000).toFixed(0), natural: mth[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()};
+      response = JSON.stringify(response);
+
+      res.send(response);
+    }
   }
 })
 
@@ -22,7 +35,15 @@ app.listen(8080, function(){
 
 function fromUnix(Unix){
   var date = new Date(+Unix * 1000);
-  var mth = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  var response = {natural: mth[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()};
+  var response = {unix: +Unix, natural: mth[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()};
   return JSON.stringify(response);
+}
+
+function dateVerify(m,d,y){
+  m = m.split('');
+  m[0] = m[0].toUpperCase();
+  m = m.join('');
+  if(y < 1970 || Number.parseInt(d) < 1) return true;
+  if(mth.indexOf(m) < 0) return true;
+  return false;
 }
